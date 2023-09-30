2da48723f043fcb62fde97a7ea129e4ec00f0dc5
Patriciof02 <122612803+Patriciof02@users.noreply.github.com>
2023-09-30 17:37:27 -0300

    correccion comillas en texto

--- src/app/proyects/page.jsx
+++ src/app/proyects/page.jsx
@@ -41,7 +41,7 @@ const page = () => {
 <div className='max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
     <div className='p-5'>
         <a href='#'>
-            <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>Proyecto Tablero de Datos ("Dashboard")</h5>
+            <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>Proyecto Tablero de Datos (Dashboard)</h5>
         </a>
         <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>Esta es una Web que muestra las ligas de fútbol a nivel mundial, donde se entregan tabla de posiciones y un gráfico para la comparación de victorias, empates y derrotas de los equipos de la liga seleccionada</p>
         <a href='https://edarvichm.github.io/Proyecto3_Grupo2/' target={'_blank'} className='inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
